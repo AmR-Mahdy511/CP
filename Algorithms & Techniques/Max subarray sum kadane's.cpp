76465e45528c83d6c525bf51c1526f05d9066666
AmR Mahdy <amrmahdy461@gmail.com>
2025-12-09 13:32:45 +0200

    Clarify initialization of ans in Kadane's algorithm
    Added a comment to clarify the initialization of the ans variable.

--- Algorithms & Techniques/Max subarray sum kadane's.cpp
+++ Algorithms & Techniques/Max subarray sum kadane's.cpp
@@ -1,5 +1,6 @@
 ll kadane(ll n , vector < ll > &v)
 {
+     // make sure make ans as a min value that can not be reached
      ll ans = INT_MIN , sum = 0;
     for(int i = 0 ; i < n ; i++){
         sum += v[i];
