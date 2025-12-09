402828f472e3d74706e5547554a5df1d8da10095
AmR Mahdy <amrmahdy461@gmail.com>
2025-12-09 13:33:07 +0200

    Set initial ans to a minimum value in max_sub_sum
    Initialize ans with a minimum value to ensure correct calculations.

--- Algorithms & Techniques/Max subarray sum prefix.cpp
+++ Algorithms & Techniques/Max subarray sum prefix.cpp
@@ -1,5 +1,6 @@
 ll max_sub_sum(ll n , vector < ll > &v)
 {
+    // make sure make ans as a min value that can not be reached
     ll ans = -1e16 , curr_sum = 0 , min_sum = 0;
     for(auto &a : v)
     {
