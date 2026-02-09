f364d6ba11ce7f9c55a3c619c9da915c09b6d33a
AmR Mahdy <amrmahdy461@gmail.com>
2026-02-09 22:06:07 +0200

    Refactor SegmentTree for improved functionality

--- Data structures/segtree.cpp
+++ Data structures/segtree.cpp
@@ -1,6 +1,13 @@
+ll oo = 1e16;
+struct Node
+{
+    ll val , idx;
+};
+
 template < typename T >
 struct SegmentTree
 {
+ 
 private:
     vector< T > seg;
     int sz;
@@ -8,17 +15,17 @@ private:
  
     T merge( T a , T b )
     {
-        return min(a,b);
+        return (a.val >= b.val ? a : b);
     }
  
     void build( int l , int r , int node , vector< T >& v )
     {
         if( l == r )
         {
-            if( l < v.size() )  seg[ node ] = v[ l ];
+            if( l < v.size() ) seg[ node ] = v[l];
             return;
         }
-        int mid = l + ( r - l ) / 2;
+        ll mid = l + ( r - l ) / 2;
         build( l , mid , 2*node+1 , v );
         build( mid+1 , r , 2*node+2 , v );
         seg[ node ] = merge( seg[ 2*node+1 ] , seg[ 2*node+2 ] );
@@ -28,7 +35,7 @@ private:
     {
         if( l == r )
         {
-            seg[ node ] = val;
+            seg[ node ] = val ;
             return;
         }
         ll mid = l + ( r - l ) / 2;
@@ -37,12 +44,12 @@ private:
         seg[ node ] = merge( seg[ 2*node+1 ] , seg[ 2*node+2 ] );
     }
  
-    T query( int l , int r , int node , int lx , int rx )
+    T query(int l, int r, int node, int lx, int rx)
     {
         if( l > rx || r < lx ) return skip;
         if( l >= lx && r <= rx ) return seg[ node ];
         ll mid = l + ( r - l ) / 2;
-        return merge( query( l , mid , 2*node+1 , lx , rx ) , query( mid+1 , r , 2*node+2 , lx , rx ) );
+        return merge(query( l , mid , 2*node+1 , lx , rx ) , query( mid+1 , r , 2*node+2 , lx , rx ));
     }
  
 public:
@@ -50,12 +57,19 @@ public:
     {
         sz = 1;
         while( sz < n ) sz <<= 1;
-        skip = T();
-        skip = 1e15;
+        // skip = T();
+        skip = Node{ -oo , 0 } ;
         seg.assign( sz << 1 , skip );
         build( 0 , sz-1 , 0 , v );
     }
- 
+    SegmentTree( int n )
+    {
+        sz = 1;
+        while( sz < n ) sz <<= 1;
+        // skip = T();
+        skip = Node{ -oo , 0 } ;
+        seg.assign( sz << 1 , skip );
+    }
     void update( int idx , T val )
     {
         update( 0 , sz-1 , 0 , idx , val );
