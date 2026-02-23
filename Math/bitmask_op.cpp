51e9c88543c76a38badac80fc2c2c28e84c5b1a8
AmR Mahdy <amrmahdy461@gmail.com>
2026-02-23 04:21:22 +0200

    Add comments to setbit and resetbit functions
    Added comments to clarify the purpose of setbit and resetbit functions.

--- Math/bitmask_op.cpp
+++ Math/bitmask_op.cpp
@@ -1,4 +1,4 @@
 bool knowbit(ll n,int i){ return (n>>i)&1LL; }
-ll setbit(ll n,int i){ return n|(1LL<<i); }
-ll resetbit(ll n,int i){ return n&(~(1LL<<i)); }
+ll setbit(ll n,int i){ return n|(1LL<<i); } // make it 1
+ll resetbit(ll n,int i){ return n&(~(1LL<<i)); } // make it zero
 ll flip(ll n,int i){ return n^(1LL<<i); }
