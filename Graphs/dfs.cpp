270c4595261aa2a3f62c7ba10467b9be117362a5
AmR Mahdy <amrmahdy461@gmail.com>
2026-03-12 16:15:32 +0200

    el flasha in dfs

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -3,83 +3,225 @@ using namespace std;
 
 // ---------- Graph ----------
 vector< vector<int> > adj; // adjacency list
-vector< int > vis;         // visited
+vector< int > vis;         // vis
 vector< int > color;       // for cycle detection
 vector< int > topo;        // for topological sort
 
-// Basic DFS
+
+// ---------- DFS ----------
 void dfs(int u)
 {
     vis[u] = 1;
-    for(int v : adj[u]){
-        if(!vis[v]) dfs(v);
+    for(auto &it : adj[u])
+    {
+        if( !vis[it] ) dfs(it);
     }
 }
 
-// Count nodes in component
-int dfs_count(int u){
+function < void (int) > dfs = [&](int node) -> void
+{
+    vis[node] = 1;
+    for(auto &it : adj[node])
+    {
+        if( !vis[it] ) dfs(it);
+    }
+};
+
+
+// ---------- Count Nodes in Component ----------
+int dfs_count(int u)
+{
     vis[u] = 1;
     int cnt = 1;
-    for(int v : adj[u]){
-        if(!vis[v]) cnt += dfs_count(v);
+
+    for(auto &it : adj[u])
+    {
+        if( !vis[it] ) cnt += dfs_count(it);
     }
+
     return cnt;
 }
 
-// Detect cycle in directed graph
-bool dfs_cycle(int u){
-    color[u] = 1; // gray
-    for(int v : adj[u]){
-        if( color[v]==0 ) // white
-        { 
-            if(dfs_cycle(v)) return 1;
+function < int (int) > dfs_count = [&](int node) -> int
+{
+    vis[node] = 1;
+    int cnt = 1;
+
+    for(auto &it : adj[node])
+    {
+        if( !vis[it] )
+        {
+            cnt += dfs_count(it);
         }
-        else if( color[v] == 1 ) return 1;
     }
+
+    return cnt;
+};
+
+
+// ---------- Number of Connected Components ----------
+int comp = 0;
+
+function < int (int) > dfs_comp = [&](int node) -> int
+{
+    vis[node] = 1;
+    int cnt = 1;
+
+    for(auto &it : adj[node])
+    {
+        if( !vis[it] )
+        {
+            cnt += dfs_comp(it);
+        }
+    }
+
+    return cnt;
+};
+
+// usage
+/*
+for(int i = 1 ; i <= n ; i++)
+{
+    if( !vis[i] )
+    {
+        dfs_comp(i);
+        comp++;
+    }
+}
+*/
+
+
+// ---------- Detect Cycle in Directed Graph ----------
+bool dfs_cycle(int u)
+{
+    color[u] = 1; // gray
+
+    for(auto &it : adj[u])
+    {
+        if( color[it] == 0 )
+        {
+            if( dfs_cycle(it) ) return 1;
+        }
+        else if( color[it] == 1 ) return 1;
+    }
+
     color[u] = 2; // black
     return 0;
 }
 
-// Topological sort (DFS)
-void dfs_topo(int u){
-    vis[u] = 1;
-    for(int v : adj[u]){
-        if(!vis[v]) dfs_topo(v);
+function < bool (int) > dfs_cycle = [&](int node) -> bool
+{
+    color[node] = 1;
+
+    for(auto &it : adj[node])
+    {
+        if( color[it] == 0 )
+        {
+            if( dfs_cycle(it) ) return 1;
+        }
+        else if( color[it] == 1 ) return 1;
     }
-    topo.push_back(u); 
+
+    color[node] = 2;
+    return 0;
+};
+
+
+// ---------- Topological Sort ----------
+void dfs_topo(int u)
+{
+    vis[u] = 1;
+
+    for(auto &it : adj[u])
+    {
+        if( !vis[it] ) dfs_topo(it);
+    }
+
+    topo.push_back(u);
 }
 
-// ---------- DFS on Grid ----------
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+function < void (int) > dfs_topo = [&](int node) -> void
+{
+    vis[node] = 1;
 
-bool is_valid(int x, int y, vector<vector<int>>& grid)
+    for(auto &it : adj[node])
+    {
+        if( !vis[it] ) dfs_topo(it);
+    }
+
+    topo.push_back(node);
+};
+
+
+// ---------- DFS on Grid ----------
+int dx[4] = {0 , 1 , 0 , -1};
+int dy[4] = {1 , 0 , -1 , 0};
+
+bool is_valid(int x , int y , vector< vector<int> > &grid)
 {
     int n = grid.size();
     int m = grid[0].size();
-    if(x<0 || y<0 || x>=n || y>=m || grid[x][y]==1) return 0;
+
+    if( x < 0 || y < 0 || x >= n || y >= m || grid[x][y] == 1 )
+        return 0;
+
     return 1;
 }
 
-void dfs_grid(int x, int y, vector<vector<int>>& grid){
-    if(!is_valid(x, y, grid)) return;
+void dfs_grid(int x , int y , vector< vector<int> > &grid)
+{
+    if( !is_valid(x , y , grid) ) return;
 
-    grid[x][y] = 1; 
+    grid[x][y] = 1;
 
-    for(int d = 0; d < 4; d++){
-        dfs_grid(x + dx[d], y + dy[d], grid);
+    for(int d = 0 ; d < 4 ; d++)
+    {
+        dfs_grid(x + dx[d] , y + dy[d] , grid);
     }
 }
 
-// ---------- DFS on Undirected Graph ----------
-bool dfs_cycle_undirected(int u, int p){
-    vis[u] = 1;
-    for(int v : adj[u])
+function < void (int , int , vector< vector<int> >& ) > dfs_grid = [&](int x , int y , vector< vector<int> > &grid) -> void
+{
+    if( !is_valid(x , y , grid) ) return;
+
+    grid[x][y] = 1;
+
+    for(int d = 0 ; d < 4 ; d++)
     {
-        if(!vis[v]){
-            if(dfs_cycle_undirected(v,u)) return 1;
-        } 
-        else if(v != p) return 1; // found cycle
+        dfs_grid(x + dx[d] , y + dy[d] , grid);
     }
+};
+
+
+// ---------- Cycle in Undirected Graph ----------
+bool dfs_cycle_undirected(int u , int p)
+{
+    vis[u] = 1;
+
+    for(auto &it : adj[u])
+    {
+        if( !vis[it] )
+        {
+            if( dfs_cycle_undirected(it , u) ) return 1;
+        }
+        else if( it != p ) return 1;
+    }
+
     return 0;
 }
+
+function < bool (int , int) > dfs_cycle_undirected = [&](int node , int par) -> bool
+{
+    vis[node] = 1;
+
+    for(auto &it : adj[node])
+    {
+        if( !vis[it] )
+        {
+            if( dfs_cycle_undirected(it , node) ) return 1;
+        }
+        else if( it != par ) return 1;
+    }
+
+    return 0;
+};
