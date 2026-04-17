8b0625a4a080cb8dbae251a08c5c96af21b94c32
AmR Mahdy <amrmahdy461@gmail.com>
2026-04-18 01:05:38 +0200

    Update bellman.cpp

--- Graphs/bellman.cpp
+++ Graphs/bellman.cpp
@@ -39,3 +39,35 @@ ll bellman(ll st)
     if(dis[n] == -1e17) return -1;
     else return dis[n];    
 }
+// ===================================================================
+const int N = 5005;
+vector < Edge > edges;
+vector < ll > dis( N , 1e18 );
+vector < bool > cycled( n + 2 , 0 );
+// do not forget edges.clear(); 
+        function < void(int) > bellman = [&](int st) -> void
+        {
+            for(int i = 0 ; i <= n ; i++) dis[i] = 1e18 , cycled[i] = 0;
+            dis[st] = 0;
+            for(int step = 1 ; step < n  ; step++)
+            {
+                for( auto &[u,v,cost] : edges )
+                {
+                    if( dis[u] + cost < dis[v] and dis[u] != 1e18 ) dis[v] = dis[u] + cost ;
+                }
+            }
+            for(int step = 1 ; step < n ; step++)
+            {
+                for( auto &[u,v,cost] : edges )
+                {
+                    if( dis[u] + cost < dis[v] and dis[u] != 1e18 ) cycled[v] = 1;
+                }
+            }          
+            for(int step = 1 ; step < n ; step++)
+            {
+                for( auto &[u,v,cost] : edges )
+                {
+                    if( cycled[u] ) cycled[v] = 1;
+                }
+            }                 
+        };
