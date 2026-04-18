e32c7e317b17062eff2c5a020de1521087480225
AmR Mahdy <amrmahdy461@gmail.com>
2026-04-18 18:38:00 +0200

    Update bellman.cpp

--- Graphs/bellman.cpp
+++ Graphs/bellman.cpp
@@ -4,40 +4,27 @@ int a,b;
 ll cost;
 };
 vector < edge > edges(N) , edges2(N);
-vector < ll > dis(N , -1e17);
+vector < ll > dis(N , 1e18);
 ll n,m;
 bool ok = 1;
 bool vis1[N] , vis2[N];
 
-ll bellman(ll st)
+ll bellman(int st)
 {
+    for(int i = 1 ; i <= n ; i++) dis[i] = 1e18;
+
     dis[st] = 0;
+
     for(int i = 1 ; i < n ; i++)
     {
         for(auto &e : edges)
         {
-            if(dis[e.a] > -1e17 &&  dis[e.b] < dis[e.a] + e.cost)
+            if(dis[e.a] != 1e18 && dis[e.a] + e.cost < dis[e.b])
             {
                 dis[e.b] = dis[e.a] + e.cost;
             }
         }
     }
-
-    for(int i = 1 ; i <= n ; i++)
-    {
-        for(auto &e : edges)
-        {
-            if(dis[e.a] > -1e17 &&  dis[e.b] < dis[e.a] + e.cost)
-            {
-                if(vis1[e.a] && vis2[e.b])
-                {
-                    return -1;
-                }
-            }
-        }
-    }
-    if(dis[n] == -1e17) return -1;
-    else return dis[n];    
 }
 // ===================================================================
 const int N = 5005;
