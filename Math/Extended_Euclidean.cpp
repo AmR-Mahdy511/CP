d4f0432e3b2d0145735b8c2f067bb42d327a2a3e
AmR Mahdy <amrmahdy461@gmail.com>
2026-04-25 16:06:02 +0300

    Update Extended_Euclidean.cpp

--- Math/Extended_Euclidean.cpp
+++ Math/Extended_Euclidean.cpp
@@ -2,6 +2,9 @@
 it solves 
 ax + by = gcd( a , b )
 to get x , y valid
+it can be used with Bézout's identity
+to solve ax + by + c = 0 
+if ans is found ( k = (-c / Gcd)) ==> x * k , y * k
 */ 
 ll Extended_Euclidean( ll a , ll b , ll &x , ll &y )
 {
