547701fe75e636649a6e390c80e8cc3378a32242
AmR Mahdy <amrmahdy461@gmail.com>
2026-07-01 16:48:10 +0300

    Create KMP_function.cpp

---
+++ Strings/KMP_function.cpp
@@ -0,0 +1,12 @@
+vector < int > KMP ( string &s )
+{
+    int n = s.size();
+    vector < int > pi(n);
+    for(int i = 1 , j = 0 ; i < n ; i++)
+    {
+        while( j and s[i] != s[j] ) j = pi[j-1];
+        if( s[i] == s[j] ) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
